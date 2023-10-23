c1e00f887e46ec5f54acd4f3cde7b290a66cc9db
Gleice Kelly <gleice.kelly22@hotmail.com>
2023-10-23 15:59:25 -0300

    Estrutura da SecaoBanner

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import './App.css';
 
 import Topo from "./componentes/Topo";
+import SecaoBanner from "./componentes/SecaoBanner";
 
 export default function App() {
   return (
     <div className="App">
       <Topo/>
+
+      <SecaoBanner/>
     </div>
   );
 }
---
+++ src/componentes/SecaoBanner/index.jsx
@@ -0,0 +1,14 @@
+import './style.css';
+
+export default function SecaoBanner () {
+    return (
+        <section>
+            <div></div>
+            <div>
+                <p>BRANDING / UI / UX / TECNOLOGIA</p>
+                <h2>Agência de Branding</h2>
+                <span>e design digital</span>
+            </div>
+        </section>
+    );
+}
