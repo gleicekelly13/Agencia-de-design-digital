b29d27d55c5996ca99949d3ff5524f610af78770
Gleice Kelly <gleice.kelly22@hotmail.com>
2023-10-24 10:26:43 -0300

    Estilização da secaoBanner

--- src/componentes/SecaoBanner/index.jsx
+++ src/componentes/SecaoBanner/index.jsx
@@ -2,9 +2,9 @@ import './style.css';
 
 export default function SecaoBanner () {
     return (
-        <section>
-            <div></div>
-            <div>
+        <section id='secaoBanner'>
+            <div id='imagemDeFundo' className='fundo-modo-claro'></div>
+            <div className='secaoBanner-textos'>
                 <p>BRANDING / UI / UX / TECNOLOGIA</p>
                 <h2>Agência de Branding</h2>
                 <span>e design digital</span>
