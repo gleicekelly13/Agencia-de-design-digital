ea78251225751dfc39f565f5cd8ff429c2189332
Gleice Kelly <gleice.kelly22@hotmail.com>
2023-10-24 16:30:16 -0300

    Estilização do Rodape

--- src/componentes/Rodape/index.jsx
+++ src/componentes/Rodape/index.jsx
@@ -9,12 +9,12 @@ import iconeGooglePlus from '../../assets/google-plus.png';
 
 export default function Rodape () {
     return (
-        <footer>
-            <img src={Logo}/>
-            <p>Ajudamos a criar uma personalidade digital construindo sua marca no ambiente online utilizando estratégias,
+        <footer className='rodape-modo-claro'>
+            <img src={Logo} alt='logomarca'/>
+            <p className='paragrafo'>Ajudamos a criar uma personalidade digital construindo sua marca no ambiente online utilizando estratégias,
             ferramentas e tecnologias personalizadas.
             </p>
-            <div>
+            <div className='iconesRodape'>
                 <img src={iconeFacebook} alt='facebook'/>
                 <img src={iconeX} alt='twitter'/>
                 <img src={iconeLinkedin} alt='linkedin'/>
@@ -22,7 +22,7 @@ export default function Rodape () {
                 <img src={iconeBehance} alt='behance'/>
                 <img src={iconeGooglePlus} alt='googleplus'/>
             </div>
-            <div>
+            <div className='copyright'>
                 <p>Copyright 2023 &copy; <span>Gleice Kelly</span></p>
             </div>
         </footer>
