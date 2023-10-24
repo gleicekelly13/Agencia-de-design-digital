1c17eb4ba27e7e7460d2a471a08f3be566f4da15
Gleice Kelly <gleice.kelly22@hotmail.com>
2023-10-24 10:04:43 -0300

    Estilização do Topo

--- src/componentes/Topo/index.jsx
+++ src/componentes/Topo/index.jsx
@@ -1,16 +1,15 @@
 import Logo from '../../assets/logo.png';
 import iconeLua from '../../assets/moon.png'
 
+import './style.css';
 
 export default function Topo () {
     return (
-        <header>
-            <div>
-                <img src={Logo} alt='logomarca'/>
-                <button>
-                    <img src={iconeLua} alt='icone-lua'/>
-                </button>
-            </div>
+        <header className='topo-modo-claro'>
+            <img className="logo" src={Logo} alt='logomarca'/>
+            <button className='btn-modo-claro'>
+                <img className='iconesBtn' src={iconeLua} alt='icone-lua'/>
+            </button>
         </header>
     );
 }
