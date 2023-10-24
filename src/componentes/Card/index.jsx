e09bdf5ed7e0bd88d4cc5722595e69a2e9df430d
Gleice Kelly <gleice.kelly22@hotmail.com>
2023-10-24 11:15:15 -0300

    Estilização dos cards

--- src/componentes/Card/index.jsx
+++ src/componentes/Card/index.jsx
@@ -2,10 +2,10 @@ import './style.css';
 
 export default function Card (props) {
     return (
-        <div>
-            <p>{props.data}</p>
+        <div id='card' className='card-modo-claro'>
+            <p className='primeiro-paragrafo'>{props.data}</p>
             <h4>{props.titulo}</h4>
-            <p>{props.empresa}</p>
+            <p className='segundo-paragrafo'>{props.empresa}</p>
             <p>{props.paragrafo}</p>
         </div>
     );
